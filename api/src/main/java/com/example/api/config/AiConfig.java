3bd587236d89bdbe19eabaea76b3082760582dd9
Hoon <hoon0661@gmail.com>
2025-11-02 12:57:05 -0500

    api - implemented RAG to loop up and inject data of resume best practices

--- api/src/main/java/com/example/api/config/AiConfig.java
+++ api/src/main/java/com/example/api/config/AiConfig.java
@@ -1,8 +1,12 @@
 package com.example.api.config;
 
 import com.example.api.advisors.TokenUsageAuditAdvisor;
+import com.example.api.rag.PIIMaskingDocumentPostProcessor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,70 +16,87 @@ import java.util.List;
 public class AiConfig {
 
     @Bean
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+    public ChatClient chatClient(
+            ChatClient.Builder chatClientBuilder,
+            RetrievalAugmentationAdvisor retrievalAugmentationAdvisor
+    ) {
         return chatClientBuilder
                 .defaultSystem(
                         """
-                        You are an expert in ATS (Applicant Tracking System) and resume analysis.
-                        The response must be data in JSON format only, without any leading or trailing text before and after the JSON data.
-                        
-                        Provide the feedback using the following format:
-                        {
-                          "overallScore": number, // max 100
-                          "ats": {
-                            "score": number, // rate based on ATS suitability (0–100)
-                            "tips": [
-                              {
-                                "type": "good" | "improve",
-                                "tip": "string", // short title
-                                "explanation": "string" //explain in detail here
-                              }
-                            ] // give 3–4 tips
-                          },
-                          "toneAndStyle": {
-                            "score": number, // max 100
-                            "tips": [
-                              {
-                                "type": "good" | "improve",
-                                "tip": "string", //make it a short "title" for the actual explanation
-                                "explanation": "string" //explain in detail here
-                              }
-                            ] // give 3–4 tips
-                          },
-                          "content": {
-                            "score": number, // max 100
-                            "tips": [
-                              {
-                                "type": "good" | "improve",
-                                "tip": "string", //make it a short "title" for the actual explanation
-                                "explanation": "string" //explain in detail here
-                              }
-                            ] // give 3–4 tips
-                          },
-                          "structure": {
-                            "score": number, // max 100
-                            "tips": [
-                              {
-                                "type": "good" | "improve",
-                                "tip": "string", //make it a short "title" for the actual explanation
-                                "explanation": "string" //explain in detail here
-                              }
-                            ] // give 3–4 tips
-                          },
-                          "skills": {
-                            "score": number, // max 100
-                            "tips": [
-                              {
-                                "type": "good" | "improve",
-                                "tip": "string", //make it a short "title" for the actual explanation
-                                "explanation": "string" //explain in detail here
-                              }
-                            ] // give 3–4 tips
-                          }
-                        }
-                        """
+                                You are an expert in ATS (Applicant Tracking System) and resume analysis.
+                                The response must be data in JSON format only, without any leading or trailing text before and after the JSON data.
+                                
+                                Provide the feedback using the following format:
+                                {
+                                  "overallScore": number, // max 100
+                                  "ats": {
+                                    "score": number, // rate based on ATS suitability (0–100)
+                                    "tips": [
+                                      {
+                                        "type": "good" | "improve",
+                                        "tip": "string", // short title
+                                        "explanation": "string" //explain in detail here
+                                      }
+                                    ] // give 3–4 tips
+                                  },
+                                  "toneAndStyle": {
+                                    "score": number, // max 100
+                                    "tips": [
+                                      {
+                                        "type": "good" | "improve",
+                                        "tip": "string", //make it a short "title" for the actual explanation
+                                        "explanation": "string" //explain in detail here
+                                      }
+                                    ] // give 3–4 tips
+                                  },
+                                  "content": {
+                                    "score": number, // max 100
+                                    "tips": [
+                                      {
+                                        "type": "good" | "improve",
+                                        "tip": "string", //make it a short "title" for the actual explanation
+                                        "explanation": "string" //explain in detail here
+                                      }
+                                    ] // give 3–4 tips
+                                  },
+                                  "structure": {
+                                    "score": number, // max 100
+                                    "tips": [
+                                      {
+                                        "type": "good" | "improve",
+                                        "tip": "string", //make it a short "title" for the actual explanation
+                                        "explanation": "string" //explain in detail here
+                                      }
+                                    ] // give 3–4 tips
+                                  },
+                                  "skills": {
+                                    "score": number, // max 100
+                                    "tips": [
+                                      {
+                                        "type": "good" | "improve",
+                                        "tip": "string", //make it a short "title" for the actual explanation
+                                        "explanation": "string" //explain in detail here
+                                      }
+                                    ] // give 3–4 tips
+                                  }
+                                }
+                                """
                 )
-                .defaultAdvisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor()))
+                .defaultAdvisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor(), retrievalAugmentationAdvisor))
+                .build();
+    }
+
+    @Bean
+    RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(VectorStore vectorStore, ChatClient.Builder chatClientBuilder) {
+        return RetrievalAugmentationAdvisor.builder()
+                .documentRetriever(
+                        VectorStoreDocumentRetriever.builder()
+                                .vectorStore(vectorStore)
+                                .topK(3)
+                                .similarityThreshold(0.5)
+                                .build()
+                )
+                .documentPostProcessors(PIIMaskingDocumentPostProcessor.builder())
                 .build();
     }
 }
